33ee84bae74a239ea5684bcf2756a133811f3fa0
Chris Gilardi <cgilardi97@gmail.com>
2018-12-27 14:14:53 -0800

    Added PlanHelp pane

---
+++ src/quote/panes/plan/plan-help.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PlanHelp extends Component {
+    render() {
+        return (
+            <div>
+                Plan Help
+            </div>
+        );
+    }
+}
--- src/quote/quote-help.js
+++ src/quote/quote-help.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
 import BeginHelp from './panes/begin/begin-help';
 import PersonalHelp from './panes/personal/personal-help';
+import PlanHelp from './panes/plan/plan-help';
 
 class QuoteHelp extends Component {
     render() {
@@ -9,6 +10,7 @@ class QuoteHelp extends Component {
             <Switch>
                 <Route path={this.props.match.path + "/begin"} component={BeginHelp}/>
                 <Route path={this.props.match.path + "/personal"} component={PersonalHelp}/>
+                <Route path={this.props.match.path + "/plan"} component={PlanHelp}/>
             </Switch>
         );
     }
