1ee6acc321e7222dda5b819e6d2f4622a8f29281
Chris Gilardi <cgilardi97@gmail.com>
2018-12-27 14:13:53 -0800

    Added Personal Help Pane

---
+++ src/quote/panes/personal/personal-help.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PersonalHelp extends Component {
+    render() {
+        return (
+            <div>
+                Personal Help
+            </div>
+        );
+    }
+}
--- src/quote/quote-help.js
+++ src/quote/quote-help.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
 import BeginHelp from './panes/begin/begin-help';
+import PersonalHelp from './panes/personal/personal-help';
 
 class QuoteHelp extends Component {
     render() {
         return (
             <Switch>
                 <Route path={this.props.match.path + "/begin"} component={BeginHelp}/>
+                <Route path={this.props.match.path + "/personal"} component={PersonalHelp}/>
             </Switch>
         );
     }
