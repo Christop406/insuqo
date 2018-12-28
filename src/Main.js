888b674cbf1017fd918eda6d85034c3df7603381
Chris Gilardi <cgilardi97@gmail.com>
2018-12-28 14:53:38 -0800

    Added Serve

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,6 @@
 import React, { Component }  from 'react';
 import Store from './ApplicationStore';
 import Quote  from './quote/quote';
-import QuoteHelp from './quote/quote-help';
 import Application from './application/application';
 import { fadeInUpBig } from 'react-animations';
 import Radium, {StyleRoot} from 'radium';
--- src/application/application.js
+++ src/application/application.js
@@ -1,11 +1,12 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React, { Component } from 'react';
 import Store from '../ApplicationStore';
+import * as qs from 'query-string';
 
 class Application extends Component {
     render = () => {
         return(
-            <div>//TODO: Implement Application</div>
+            <div>{JSON.stringify(qs.parse(this.props.location.search, {ignoreQueryPrefix: true}))}</div>
         );
     };
 }
