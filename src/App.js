5634adb94a8ebf5cd894000c41fb3980eec258dc
Chris Gilardi <cgilardi97@gmail.com>
2019-04-16 12:03:27 -0700

    Trim the fat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Store from './ApplicationStore';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import HttpsRedirect from 'react-https-redirect';
 import Main from "./Main";
 import './App.css';
 
@@ -9,15 +8,13 @@ import './App.css';
 class App extends React.Component {
     render = () => {
         return (
-            <HttpsRedirect>
-                <Store.Container>
-                    <Router>
-                        <Switch>
-                            <Route path="/" component={Main}/>
-                        </Switch>
-                    </Router>
-                </Store.Container>
-            </HttpsRedirect>
+            <Store.Container>
+                <Router>
+                    <Switch>
+                        <Route path="/" component={Main}/>
+                    </Switch>
+                </Router>
+            </Store.Container>
         )
     }
 }
