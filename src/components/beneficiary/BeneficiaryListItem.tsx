d10c7bde176a6711c608a2f6ef857ce10b0f1ba9
Chris Gilardi <cgilardi97@gmail.com>
2020-02-19 00:01:19 -0800

    Added email to beneficiary

--- src/components/beneficiary/BeneficiaryListItem.tsx
+++ src/components/beneficiary/BeneficiaryListItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Beneficiary } from '@insuqo/shared/types/application';
 import s from './BeneficiaryListItem.module.scss';
 import relations from '@insuqo/shared/constants/relations.json';
@@ -17,12 +17,15 @@ interface BeneficiaryListItemProps {
 
 const BeneficiaryListItem: React.FC<BeneficiaryListItemProps> = ({ onClose, onChange, beneficiary }) => {
 
+    onChange = useCallback(onChange as any, []);
+
     const [firstName, setFirstName] = useState(beneficiary.firstName);
     const [middleInitial, setMiddleInitial] = useState(beneficiary.middleInitial);
     const [lastName, setLastName] = useState(beneficiary.lastName);
     const [relationship, setRelationship] = useState(beneficiary.relationship);
     const [percentage, setPercentage] = useState(beneficiary.percentage);
     const [suffix, setSuffix] = useState(beneficiary.suffix);
+    const [email, setEmail] = useState(beneficiary.email);
 
     useEffect(() => {
         onChange && onChange({
@@ -31,9 +34,10 @@ const BeneficiaryListItem: React.FC<BeneficiaryListItemProps> = ({ onClose, onCh
             lastName,
             percentage,
             relationship,
-            suffix
+            suffix,
+            email,
         });
-    }, [firstName, middleInitial, lastName, percentage, relationship]);
+    }, [firstName, middleInitial, lastName, percentage, relationship, email, onChange, suffix]);
 
     useEffect(() => {
         setFirstName(beneficiary.firstName);
@@ -42,40 +46,48 @@ const BeneficiaryListItem: React.FC<BeneficiaryListItemProps> = ({ onClose, onCh
         setRelationship(beneficiary.relationship);
         setPercentage(beneficiary.percentage);
         setSuffix(beneficiary.suffix);
+        setEmail(beneficiary.email);
     }, [beneficiary]);
 
     return (
         <div className={s.container}>
-            <div className={s.infoContainer}>
-                <button onClick={onClose} type="button" className={cx(s.deleteButton, 'button')}>
-                    <FontAwesomeIcon icon={['far', 'times-circle']} />
-                </button>
-                <div>
-                    <div className={s.beneficiaryName}>
-                        <EditableLabel 
-                            value={firstName}
-                            onChange={setFirstName as any} 
-                            placeholder="First" />
-                        <EditableLabel 
-                            value={middleInitial}
-                            onChange={setMiddleInitial as any} 
-                            placeholder="M" />
-                        <EditableLabel 
-                            value={lastName}
-                            onChange={setLastName as any} 
-                            placeholder="Last" />
+            <button onClick={onClose} type="button" className={cx(s.deleteButton, 'button')}>
+                <FontAwesomeIcon icon={['far', 'times-circle']} />
+            </button>
+            <div className={s.content}>
+                <div className={s.personalContainer}>
+                    <div className={s.infoContainer}>
+                        <div>
+                            <div className={s.beneficiaryName}>
+                                <EditableLabel
+                                    value={firstName}
+                                    onChange={setFirstName as any}
+                                    placeholder="First" />
+                                <EditableLabel
+                                    value={middleInitial}
+                                    onChange={setMiddleInitial as any}
+                                    placeholder="M" />
+                                <EditableLabel
+                                    value={lastName}
+                                    onChange={setLastName as any}
+                                    placeholder="Last" />
+                            </div>
+                            <select placeholder="Relationship" value={relationship} onChange={setRelationship as any} className={cx(s.selector, 'select input')}>
+                                {relations.map((r) => <option key={r.code} value={r.code}>{r.name}</option>)}
+                            </select>
+                        </div>
                     </div>
-                    <select placeholder="Relationship" value={relationship} onChange={setRelationship as any} className={cx(s.selector, 'select input')}>
-                        {relations.map((r) => <option key={r.code} value={r.code}>{r.name}</option>)}
-                    </select>
+                    <div className={s.shareContainer}>
+                        <EditableLabel
+                            value={percentage}
+                            onChange={setPercentage as any}
+                            className={s.shareLabel}
+                            placeholder="20" />%
+                    </div>
                 </div>
-            </div>
-            <div className={s.shareContainer}>
-                <EditableLabel 
-                    value={percentage} 
-                    onChange={setPercentage as any} 
-                    className={s.shareLabel} 
-                    placeholder="20" />%
+                <div className={s.contactContainer}>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" className={cx('input')} />
+                </div>
             </div>
         </div>
     );
